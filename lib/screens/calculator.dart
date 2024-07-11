5efe8811327811dc55a0f2c222475e01361d3b2d
codexqui <codexqui@outlook.com>
2024-07-11 03:48:26 -0500

    second commit

--- lib/screens/calculator.dart
+++ lib/screens/calculator.dart
@@ -1,139 +1,135 @@
 import 'package:flutter/material.dart';
 
 class CalculatorScreen extends StatefulWidget {
-  const CalculatorScreen({super.key});
+  const CalculatorScreen({Key? key}) : super(key: key);
 
   @override
   State<CalculatorScreen> createState() => _CalculatorScreenState();
 }
 
 class _CalculatorScreenState extends State<CalculatorScreen> {
-  int result = 0;
-  int number1 = 0;
-  int number2 = 0;
+  int _result = 0;
+  int _number1 = 0;
+  int _number2 = 0;
+  final TextEditingController _controller1 = TextEditingController();
+  final TextEditingController _controller2 = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _controller1.text = _number1.toString();
+    _controller2.text = _number2.toString();
+  }
+
+  @override
+  void dispose() {
+    _controller1.dispose();
+    _controller2.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
+        backgroundColor: Color.fromARGB(255, 65, 65, 65),
         centerTitle: true,
         title: const Text(
-          'Calculadora',
+          'Flutter Calculator App',
           style: TextStyle(color: Colors.white),
         ),
       ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(15, 15, 15, 0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.lightGreen,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: TextField(
-                      controller: TextEditingController(text: number1.toString()),
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(fontSize: 45),
-                      keyboardType: TextInputType.number,
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        contentPadding: EdgeInsets.all(10),
-                      ),
-                      onChanged: (value) {
-                        setState(() {
-                          number1 = int.tryParse(value) ?? 0;
-                        });
-                      },
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(15, 15, 15, 0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.lightGreen,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: TextField(
-                      controller: TextEditingController(text: number2.toString()),
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(fontSize: 45),
-                      keyboardType: TextInputType.number,
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        contentPadding: EdgeInsets.all(10),
-                      ),
-                      onChanged: (value) {
-                        setState(() {
-                          number2 = int.tryParse(value) ?? 0;
-                        });
-                      },
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              IconButton(
-                iconSize: 40,
-                onPressed: () => operar('+'),
-                icon: const Icon(Icons.add, size: 70),
-              ),
-              IconButton(
-                iconSize: 40,
-                onPressed: () => operar('-'),
-                icon: const Icon(Icons.remove, size: 70),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              IconButton(
-                iconSize: 70,
-                onPressed: () => operar('*'),
-                icon: const Icon(Icons.close, size: 70),
-              ),
-              IconButton(
-                iconSize: 70,
-                onPressed: () => operar('/'),
-                icon: const Icon(Icons.auto_fix_off, size: 70),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Text(
-              'Resultado: $result',
-              style: const TextStyle(fontSize: 24),
+      body: Container(
+        color: Color.fromARGB(255, 10, 10, 10), // Fondo de color oscuro
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(child: _buildNumberInput(_controller1, (value) => _number1 = value)),
+                const SizedBox(width: 16),
+                Expanded(child: _buildNumberInput(_controller2, (value) => _number2 = value)),
+              ],
             ),
-          ),
-        ],
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildOperationButton('+', Icons.add),
+                _buildOperationButton('-', Icons.remove),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildOperationButton('*', Icons.close),
+                _buildOperationButton('/', Icons.auto_fix_off),
+              ],
+            ),
+            const SizedBox(height: 40),
+            Text(
+              'Result: $_result',
+              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+            ),
+            const Spacer(),
+            const Text(
+              'Desarrollo en proceso por: CodexQUI',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
+            ),
+          ],
+        ),
       ),
     );
   }
 
-  void operar(String operando) {
+  Widget _buildNumberInput(TextEditingController controller, Function(int) onChanged) {
+    return TextField(
+      controller: controller,
+      textAlign: TextAlign.center,
+      style: const TextStyle(fontSize: 45, color: Colors.white),
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Color.fromARGB(255, 65, 65, 65),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+        contentPadding: const EdgeInsets.symmetric(vertical: 16),
+      ),
+      onChanged: (value) {
+        setState(() {
+          onChanged(int.tryParse(value) ?? 0);
+        });
+      },
+    );
+  }
+
+  Widget _buildOperationButton(String operation, IconData icon) {
+    return ElevatedButton(
+      onPressed: () => _operate(operation),
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Color.fromARGB(255, 117, 10, 117),
+        padding: const EdgeInsets.all(20),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      ),
+      child: Icon(icon, size: 40, color: Colors.white),
+    );
+  }
+
+  void _operate(String operation) {
     setState(() {
-      switch (operando) {
+      switch (operation) {
         case '+':
-          result = number1 + number2;
+          _result = _number1 + _number2;
+          break;
         case '-':
-          result = number1 - number2;
+          _result = _number1 - _number2;
+          break;
         case '*':
-          result = number1 * number2;
+          _result = _number1 * _number2;
+          break;
         case '/':
-          result = number1 ~/ number2;
+          _result = _number2 != 0 ? _number1 ~/ _number2 : 0;
+          break;
       }
     });
   }
