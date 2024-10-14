4c7a07239327ac4a7bdd9c851bf61cb3e739eb94
codexqui <codexqui@outlook.com>
2024-10-14 05:04:13 -0500

    A minimalist Flutter calculator app in its final version

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_calculator_app/screens/calculator.dart';
+import 'package:minimalist_calculator/screens/calculator.dart';
 
 void main() {
   runApp(const MyApp());
